59786b151e1dcf89b85a87504242f27e2a19bf13
erkanaltinors <erkanaltinors10@gmail.com>
2024-02-14 22:32:53 +0300

    update style

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,28 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
  trailingSlash: "never",
  image: {
-  remotePatterns: [{ protocol: "https" }],
+  remotePatterns: [
+   {
+    protocol: "https",
+   },
+  ],
  },
  integrations: [
   tailwind({
    applyBaseStyles: false,
   }),
   react(),
+  icon({
+   include: {
+    ph: ["arrow-up-right"],
+   },
+  }),
  ],
  build: {
   format: "file",
