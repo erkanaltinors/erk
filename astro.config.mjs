b1de5d43a0a6842d447cc940f8a6da379d8d5b1d
erkanaltinors <erkanaltinors10@gmail.com>
2024-02-14 20:33:38 +0300

    add trailingslash feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+ trailingSlash: "never",
  image: {
   remotePatterns: [{ protocol: "https" }],
  },
