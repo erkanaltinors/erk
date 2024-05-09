a9c705c678b1fc20c86e61b589b9344437d9f491
erkanaltinors <erkanaltinors10@gmail.com>
2024-05-09 21:01:02 +0300

    add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,30 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	trailingSlash: "never",
-	markdown: {
-		shikiConfig: {
-			wrap: true,
-		},
-	},
-	image: {
-		remotePatterns: [
-			{
-				protocol: "https",
-			},
-		],
-	},
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		icon({
-			include: {
-				ph: ["arrow-up-right"],
-			},
-		}),
-	],
-	build: {
-		format: "file",
-	},
+  trailingSlash: "never",
+  markdown: {
+    shikiConfig: {
+      wrap: true
+    }
+  },
+  image: {
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  },
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), icon({
+    include: {
+      ph: ["arrow-up-right"]
+    }
+  }), sitemap()],
+  build: {
+    format: "file"
+  }
 });
