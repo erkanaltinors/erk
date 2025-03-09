099cfc38383e68cc5038a3c2f906088ff37f313d
erkanaltinors <erkanaltinors10@gmail.com>
2025-03-09 23:22:18 +0300

    add modified date

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { remarkModifiedTime } from "./remark-modified-time.mjs";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://erkanaltinors.com",
@@ -11,6 +13,7 @@ export default defineConfig({
 			wrap: true,
 			theme: "dark-plus",
 		},
+		remarkPlugins: [remarkModifiedTime],
 	},
 	image: {
 		remotePatterns: [
